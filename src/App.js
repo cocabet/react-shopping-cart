8d31f41e909bb59e24eb4cd1f181d5565a281f3e
Oscar <cocabet@gmail.com>
2020-08-07 14:07:37 -0500

    add redux to filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/Cart';
@@ -11,10 +10,7 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-      size: "",
-      sort: ""
     };
   }
   createOrder = (order) =>{
@@ -48,41 +44,6 @@ class App extends React.Component {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
-  sortProducts = (event)=>{
-    const sort = event.target.value;
-    this.setState((state)=>({
-      sort: sort,
-      products : this.state.products
-        .slice()
-        .sort((a,b) =>
-        sort === "lowest"
-        ? a.price > b.price
-          ? 1
-          : -1
-        : sort === "highest"
-          ? a.price < b.price
-            ? 1
-            :-1
-          : a._id > b._id
-          ? 1
-          : -1
-      ),
-    }));
-  }
-
-  filterProducts = (event) => {
-    if(event.target.value === ""){
-      this.setState({size: event.target.value, product: data.products});
-    }else{
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-        (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }   
-  };
-
   render(){
     return (
       <Provider store={store}>
@@ -93,15 +54,8 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter 
-                count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
-              />
-              <Products 
-                products={this.state.products}
+              <Filter />
+              <Products               
                 addToCart={this.addToCart}
               >                
               </Products>
